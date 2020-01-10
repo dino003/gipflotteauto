e85605ed69ec21f031649cae3cc6e957051a329e
Boris DJOMAN <boris03djoman@gmail.com>
2020-01-10 16:14:07 +0000

    test new ci

--- app/Http/Controllers/UtilController.php
+++ app/Http/Controllers/UtilController.php
@@ -9,25 +9,25 @@ use Symfony\Component\Process\Process;
 
 class UtilController extends Controller
 {
-    public function deploy(Request $request)
-    {
-        $githubPayload = $request->getContent();
-        $githubHash = $request->header('X-Hub-Signature');
+    // public function deploy(Request $request)
+    // {
+    //     $githubPayload = $request->getContent();
+    //     $githubHash = $request->header('X-Hub-Signature');
  
-        $localToken = config('app.deploy_secret');
-        $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
+    //     $localToken = config('app.deploy_secret');
+    //     $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
  
-        if (hash_equals($githubHash, $localHash)) {
-             $root_path = base_path();
-             $process = new Process('cd ' . $root_path . '; ./deploy.sh');
-             $process->run(function ($type, $buffer) {
-               //  echo $buffer;
-                 return response()->json($buffer);
+    //     if (hash_equals($githubHash, $localHash)) {
+    //          $root_path = base_path();
+    //          $process = new Process('cd ' . $root_path . '; ./deploy.sh');
+    //          $process->run(function ($type, $buffer) {
+    //            //  echo $buffer;
+    //              return response()->json($buffer);
 
-             });
+    //          });
 
-        }
-    }
+    //     }
+    // }
 
     public function deploy_autre(Request $request){
         $gitUser = 'dino003';
